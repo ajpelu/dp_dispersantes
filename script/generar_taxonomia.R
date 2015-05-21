7065e902b5f0c91e4bc32e0f424b25c0e4df7781
lolaalr <lolaalvarez.r@gmail.com>
2015-05-21 20:26:25 +0200

    Script de generación de taxonomía con Taxize

--- script/generar_taxonomia.R
+++ script/generar_taxonomia.R
@@ -15,7 +15,7 @@ tnrs(nombre_cientifico) ##get correct spellings. Corrige los que están mal. Me 
 
 classification(datos$nombre_cientifico, db = 'itis') ##me da la clasificación de cada especie y el id
 
-out<- tax_name(query = datos$nombre_cientifico, get = "family", db = "ncbi")## me da solo el rango taxonómico que le indique. Subkingdom, infrakingdom y superclase no existen.
+family<- tax_name(query = datos$nombre_cientifico, get = "family", db = "ncbi")## me da solo el rango taxonómico que le indique. Subkingdom, infrakingdom y superclase no existen.
 
 genus<- tax_name(query = datos$nombre_cientifico, get = "genus", db = "ncbi")
 
@@ -30,18 +30,33 @@ phylum<- tax_name(query = datos$nombre_cientifico, get = "phylum", db = "ncbi")
 kingdom<- tax_name(query = datos$nombre_cientifico, get = "kingdom", db = "ncbi")
 
 
-
-
 #juntamos las dos bases de datos con la función merge().
 ##Para unir los dos data frames, tienen que tener una columna en común. Por eso añado al data frame "datos" una columna llamada query.
 
 query=vector(mode='character', length=5)
 query<- c(1:90)
 datos2=data.frame(datos,query)
-
-taxonomy <- merge(x = datos2, y = genus[c("genus")], by = "query", all.y = T)
-
+datos2$query <- as.character (datos2$query)
 
 
+taxonomy<- merge(x = datos2, y = genus[c("genus")], by = "query", all.y = T)## Lo quería hacer así para juntar solo la columna "genus", pero me da error, así que junto las dos tablas y luego eliminaré las columnas que sobren. 
+taxonomy <- merge(datos2, genus, by ="query", all.y = T)
+taxonomy <- merge(taxonomy, family, by ="query", all.y = T)
+taxonomy <- merge(taxonomy, order, by ="query", all.y = T)
+taxonomy <- merge(taxonomy, class, by ="query", all.y = T)
+taxonomy <- merge(taxonomy, subphylum, by ="query", all.y = T)
+taxonomy <- merge(taxonomy, phylum, by ="query", all.y = T)
+taxonomy <- merge(taxonomy, kingdom, by ="query", all.y = T)
 
+##Eliminar filas que sobran
+taxonomy<- taxonomy[ , -23]
+taxonomy<- taxonomy[ , -21]
+taxonomy<- taxonomy[ , -19]
+taxonomy<- taxonomy[ , -17]
+taxonomy<- taxonomy[ , -15]
+taxonomy<- taxonomy[ , -13]
+taxonomy<- taxonomy[ , -11]
 
+##guardar como csv
+
+write.csv(taxonomy, file = "taxonomy.csv")
